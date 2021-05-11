ad207287427cb883834644b56d9da1c509749d1e
Carms Ng <carmenhyng89@gmail.com>
2021-05-11 13:46:27 -0400

    [UPD] namespace from team -> team.fr

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function IndexPage() {
 
 export const query = graphql`
   query($language: String!) {
-    locales: allLocale(filter: {ns: {in: ["index", "team"]}, language: {eq: $language}}) {
+    locales: allLocale(filter: {ns: {in: ["index", "team.fr"]}, language: {eq: $language}}) {
       edges {
         node {
           ns
